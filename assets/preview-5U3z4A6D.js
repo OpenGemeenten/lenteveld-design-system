b9d380ba21080b5e0e3c9307727242cf60aabc20
Robbert <mail@robbertbroersma.nl>
2023-12-01 12:03:24 +0000

    Deploying to gh-pages from @ OpenGemeenten/lenteveld-design-system@f8dddc57dcb308cbddde462e31d64ad511b1a129 🚀

--- assets/preview-5U3z4A6D.js
+++
@@ -1,7 +0,0 @@
-import{_ as e}from"./iframe-4EajIxNn.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-c1gkqetd.js"),__vite__mapDeps([0,1,2,3,4,5]),import.meta.url);return new r}}};export{a as parameters};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-c1gkqetd.js","./chunk-HLWAVYOI-AvmjmGvo.js","./iframe-4EajIxNn.js","./index-DogsOklH.js","./react-18-qZm_vjb7.js","./index-83LIMwj7.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
